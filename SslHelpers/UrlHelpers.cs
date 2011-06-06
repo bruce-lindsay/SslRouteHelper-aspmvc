0cad28248b628e7fdba2dc264bd0aa7862e9a6fb
Bruce Lindsay <skynet1024@gmail.com>
2011-06-06 09:01:47 -0400

    implement remaining RouteUrl helpers

--- SslHelpers/UrlHelpers.cs
+++ SslHelpers/UrlHelpers.cs
@@ -3,23 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web.Mvc;
+using RouteValueDictionary = System.Web.Routing.RouteValueDictionary;
 
 namespace SslHelpers
 {
     public static class UrlHelpers
     {
-        public static string SslRouteUrl(this UrlHelper urlHelper, string routeName)
+        private static bool TryGetProctocol(string routeName, out string protocol)
         {
+            bool getProcotol = false;
             bool? useSsl = RouteOptions.useSsl(routeName);
+
             if (useSsl == true)
             {
-                return urlHelper.RouteUrl(routeName, null, "https");
+                getProcotol = true;
+                protocol = "https";
             }
             else if (useSsl == false)
             {
-                return urlHelper.RouteUrl(routeName, null, "http");
+                getProcotol = true;
+                protocol = "http";
             }
-            return urlHelper.RouteUrl(routeName);
+            else
+            {
+                protocol = null;
+            }
+
+            return getProcotol;
+        }
+
+        public static string SslRouteUrl(this UrlHelper urlHelper, string routeName)
+        {
+            return SslRouteUrl(urlHelper, routeName, null);
+        }
+
+        public static string SslRouteUrl(this UrlHelper urlHelper, string routeName, object routeValues)
+        {
+            string protocol, routeUrl;
+
+            if(TryGetProctocol(routeName, out protocol))
+            {
+                routeUrl = urlHelper.RouteUrl(routeName, null, protocol);
+            }
+            else
+            {
+                routeUrl = urlHelper.RouteUrl(routeName, null);
+            }
+
+            return routeUrl;
+        }
+
+        public static string SslRouteUrl(this UrlHelper urlHelper, string routeName, RouteValueDictionary routeValues)
+        {
+            string protocol, routeUrl;
+
+            if(TryGetProctocol(routeName, out protocol))
+            {
+                routeUrl = urlHelper.RouteUrl(routeName, routeValues, protocol, null);
+            }
+            else
+            {
+                routeUrl = urlHelper.RouteUrl(routeName, routeValues);
+            }
+
+            return routeUrl;
         }
     }
 }
